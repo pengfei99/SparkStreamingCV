660622945c35ec4135d8cb521a654b8855205065
pengfei <liu.pengfei@hotmail.fr>
2021-06-04 11:08:02 +0200

    add export conda support

---
+++ helper/CleanRequirementFile.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+
+def clean_poetry_generated_requirement_file(spark, input_file_path, output_file_path):
+    raw = spark.read.option("delimiter", ";").csv(input_file_path).toDF("dependency", "python-version")
+    # raw.show(5, False)
+    dep = raw.select("dependency")
+    dep.show(50, False)
+    dep.coalesce(1).write.format("com.databricks.spark.csv").option("header", "false").save(output_file_path)
+
+
+def main():
+    spark = SparkSession.builder.master("local").appName("test").getOrCreate()
+    input_file_path = "/requirements.txt"
+    output_file_path = "/tmp/requirement.txt"
+    clean_poetry_generated_requirement_file(spark,input_file_path,output_file_path)
+
+if __name__ == "__main__":
+    main()
